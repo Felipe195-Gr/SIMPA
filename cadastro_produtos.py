2ff562ec26e96ef237024e83121042bfd0037cd3
Uriel <uriellongo0@gmail.com>
2026-06-17 20:32:36 -0300

    mudança no nome

---
+++ cadastro_produtos.py
@@ -0,0 +1,8 @@
+def cadastrar_produto(nome_produto, descricao_produto, preco_produto):
+    print("Cadastro de Produtos")
+    nome_produto = input("Digite o nome do produto: ")
+    descricao_produto = input("Digite a descrição do produto: ")
+    preco_produto = input("Digite o preço do produto: ")
+    print(f"O nome do produto é: {nome_produto}")
+    print(f"A descrição do produto é: {descricao_produto}")
+    print(f"O preço do produto é: {preco_produto}")
