1d279277ba962d0db375ff047333cb7f6c3e449b
Felipe Grando <felipegrando07@gmail.com>
2026-06-17 20:14:51 -0300

    Fiz o menu

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-import flet as ft
+while True:
+    print("Olá, seja bem-vindo ao SIMPA!")
+    print("Esse é um sistema de MARKETPLACE de produtos artesanais")
+    print(" 1 - Cadastrar produto")
+    print(" 2 - Cadastrar clientes")
+    print(" 3 - Cadastrar vendedores")
+    print(" 4 - Listar produtos")
+    print(" 5 - Sair")
+
+    escolha = input("Digite o número da opção desejada: ")
