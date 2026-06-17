f3af1fc70b85175aa288b7b2115ff3c510b767ec
Uriel <uriellongo0@gmail.com>
2026-06-17 20:33:45 -0300

    coloquei código

--- cadastro_produtos.py
+++ cadastro_produtos.py
@@ -6,3 +6,4 @@ def cadastrar_produto(nome_produto, descricao_produto, preco_produto):
     print(f"O nome do produto é: {nome_produto}")
     print(f"A descrição do produto é: {descricao_produto}")
     print(f"O preço do produto é: {preco_produto}")
+
