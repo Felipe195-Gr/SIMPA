084c07bfdf9bdd59812c7b6e15c6d592a114dc2b
Felipe Grando <felipegrando07@gmail.com>
2026-06-17 21:18:35 -0300

    s

--- cadastro_produtos.py
+++ cadastro_produtos.py
@@ -3,6 +3,7 @@ def cadastro_produtos():
     nome_produto = input("Digite o nome do produto: ")
     descricao_produto = input("Digite a descrição do produto: ")
     preco_produto = float(input("Digite o preço do produto: "))
-    with open(f'C:\\Users\\uriel61918066\\Documents\\SIMPA\\cadastro_produtos.csv', 'a') as arquivo:
+
+    with open(f'C:\\Users\\felip\\OneDrive\\Documentos\\SIMPAcadastro_produtos.csv', 'a') as arquivo:
         arquivo.write(f"{nome_produto};{descricao_produto}; R${preco_produto:.2f}\n")
     print("Produto cadastrado com sucesso!")
