43ccf23c123474379169d0a1ab3767de5255d8d4
Uriel <uriellongo0@gmail.com>
2026-06-18 19:45:36 -0300

    adicionei algumas funcoes

--- pedidos.py
+++ pedidos.py
@@ -4,7 +4,7 @@ produto = input("Digite o nome do produto: ")
 def pedidos():
     while True:
         produto = input("Digite o nome do produto: ")
-        with open('C:\\Users\\uriel\\OneDrive\\Documentos\\Programação\\SIMPA\\cadastro_produtos.csv', 'r') as arquivo:
+        with open('C:\\Users\\uriel\\OneDrive\\Documentos\\Programação\\SIMPA\\cadastro_produtos.csv', 'r') as arquivo: 
             arquivo.read()
             print(f"{produto}")
             
