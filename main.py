c297babe2501d8eb0603b639f2cdee322920425e
Felipe Grando <felipegrando07@gmail.com>
2026-06-18 18:55:54 -0300

    mudança no menu

--- main.py
+++ main.py
@@ -5,22 +5,30 @@ from cadastro_vendedores import cadastro_vendedores
 while True:
     print("Olá, seja bem-vindo ao SIMPA!")
     print("Esse é um sistema de MARKETPLACE de produtos artesanais")
-    print(" 1 - Cadastrar produto")
-    print(" 2 - Cadastrar clientes")
-    print(" 3 - Cadastrar vendedores")
+    print(" 1 - Aba de cadastros")
     print(" 5 - Sair")
 
     escolha = input("Digite o número da opção desejada: ")
 
     if escolha == "1":
-        cadastro_produtos()
 
-    elif escolha == "2":
-        cadastro_cliente()
-    
-    elif escolha == "3":
-        cadastro_vendedores()   
+        while True:
+            print("Essa é a aba de cadastros")
+            print("Cadastro produtos")
+            print("Cadastro clientes")
+            print("Cadastro vendedores")
 
-    elif escolha == "5":
-        print("Obrigado por usar o SIMPA! Até a próxima!")
-        break
+            escolha == input("Digite o número da opção desejada: ")
+            
+            if escolha == "1":
+                cadastro_produtos()
+
+            elif escolha == "2":
+                cadastro_cliente()
+            
+            elif escolha == "3":
+                cadastro_vendedores()   
+
+    # elif escolha == "5":
+    #     print("Obrigado por usar o SIMPA! Até a próxima!")
+    #     break
