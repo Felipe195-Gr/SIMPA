a5d844792a98b0187c9834ffb5736710ccb5c75d
Uriel <uriellongo0@gmail.com>
2026-06-18 20:14:36 -0300

    finalizando testes para encontrar produto no csv

--- pedidos.py
+++ pedidos.py
@@ -1,15 +1,19 @@
-produto = input("Digite o nome do produto: ")
-
-
 def pedidos():
     while True:
         produto = input("Digite o nome do produto: ")
-        with open('C:\\Users\\uriel\\OneDrive\\Documentos\\Programação\\SIMPA\\cadastro_produtos.csv', 'r') as arquivo: 
-            arquivo.read()
+        with open('C:\\Users\\uriel\\OneDrive\\Documentos\\Programação\\SIMPA\\cadastro_produtos.csv', 'r', encoding="utf-8") as arquivo: 
             for linha in arquivo:
                 linha = linha.strip()
-                dados = linha.split(",")
+                dados = linha.split(";")
                 codigo = dados[0]
                 nome_produto = dados[1]
-            print(f"produto pesquisado: {produto}, {codigo}, {nome_produto}")
+                categoria = dados[2]
+                preco = dados[3]
+                estoque = dados[4]
+                vendedor = dados[5]
+                if produto == nome_produto:
+                    print(f"produto pesquisado: {produto}, {codigo}, {nome_produto}, {categoria}, {preco}, {estoque}, {vendedor}")
+        break
+
+pedidos()
             
