39ae7c3d244b05fab9341e23b9757dd54a4f99ad
Felipe Grando <felipegrando07@gmail.com>
2026-06-17 21:29:41 -0300

    os : foram adicionados

--- cadastro_clientes.py
+++ cadastro_clientes.py
@@ -2,7 +2,7 @@ def cadastro_cliente():
     print("Cadastro de Clientes")
     nome = input("Digite o nome do cliente: ")
     email = input("Digite o email do cliente: ")
-    endereco = input("Digite o endereço do cliente")
+    endereco = input("Digite o endereço do cliente: ")
 
     with open ('clientes.csv', 'a', encoding='utf-8') as arquivo:
         arquivo.write(f"{nome};{email};{endereco}\n")
