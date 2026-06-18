324050d4e51e405e302ebeca6b74b8d5e81ab7e8
Uriel <uriellongo0@gmail.com>
2026-06-18 19:54:00 -0300

    mudei a funcao de localizar o produto no0 cadasro_produtos.csv.

--- pedidos.py
+++ pedidos.py
@@ -6,5 +6,10 @@ def pedidos():
         produto = input("Digite o nome do produto: ")
         with open('C:\\Users\\uriel\\OneDrive\\Documentos\\Programação\\SIMPA\\cadastro_produtos.csv', 'r') as arquivo: 
             arquivo.read()
-            print(f"{produto}")
+            for linha in arquivo:
+                linha = linha.strip()
+                dados = linha.split(",")
+                codigo = dados[0]
+                nome_produto = dados[1]
+            print(f"produto pesquisado: {produto}, {codigo}, {nome_produto}")
             
