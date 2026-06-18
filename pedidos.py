95e0edb036e8db8e15a2d616e3f07e17de6bd4ca
Uriel <uriellongo0@gmail.com>
2026-06-18 19:40:17 -0300

    teste para ver se o  pedidos.py está lendo o arquivo cadastro_produtos

--- pedidos.py
+++ pedidos.py
@@ -3,9 +3,8 @@ produto = input("Digite o nome do produto: ")
 
 def pedidos():
     while True:
-        print("-------Menu de pedidos-------")
+        produto = input("Digite o nome do produto: ")
         with open('C:\\Users\\uriel\\OneDrive\\Documentos\\Programação\\SIMPA\\cadastro_produtos.csv', 'r') as arquivo:
             arquivo.read()
-            print("1 - Mostrar produtos")
+            print(f"{produto}")
             
-produto = input("Digite o nome do produto: ")
