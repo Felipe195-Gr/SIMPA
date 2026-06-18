863dba372d3a2f666657652905bf492dc66c2bb1
Uriel <uriellongo0@gmail.com>
2026-06-18 19:36:26 -0300

    estou adicionando a funcao de ler o csv de produtos

--- pedidos.py
+++ pedidos.py
@@ -1,2 +1,11 @@
+produto = input("Digite o nome do produto: ")
+
+
 def pedidos():
-    print("teste")
+    while True:
+        print("-------Menu de pedidos-------")
+        with open('C:\\Users\\uriel\\OneDrive\\Documentos\\Programação\\SIMPA\\cadastro_produtos.csv', 'r') as arquivo:
+            arquivo.read()
+            print("1 - Mostrar produtos")
+            
+produto = input("Digite o nome do produto: ")
