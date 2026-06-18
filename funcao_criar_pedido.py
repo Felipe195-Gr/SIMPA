d7d895cf0de163d413017e74a5afafd3c989da83
Uriel <uriellongo0@gmail.com>
2026-06-18 20:49:56 -0300

    acabei a funcao teste para criar pedido no csv

--- funcao_criar_pedido.py
+++ funcao_criar_pedido.py
@@ -1,4 +1,6 @@
 def criar_pedido():
     while True:
         pedido = input("Digite o pedido que deseja criar: ")
-        with open('')
+        with open('pedidos.csv', 'a', encoding='utf-8') as arquivo:
+            arquivo.write(f"{pedido}\n")
+            print("Pedido criado com sucesso!")
