541166d62ec8d507d874fe624e20536f149a4ec9
Felipe Grando <felipegrando07@gmail.com>
2026-06-17 21:08:59 -0300

    funções cadastro clientes funcionando

--- cadastro_clientes.py
+++ cadastro_clientes.py
@@ -1,8 +1,8 @@
-def cadastro_cliente(nome, email, endereço):
+def cadastro_cliente():
     print("Cadastro de Clientes")
     nome = input("Digite o nome do cliente: ")
     email = input("Digite o email do cliente: ")
-    endereço = input("Digite o endereço do cliente")
+    endereco = input("Digite o endereço do cliente")
 
     with open ('C:\\Users\\felip\\OneDrive\\Documentos\\SIMPA\\clientes.csv', 'a', encoding='utf-8') as arquivo:
-        nome, email, endereço
+        arquivo.write(f"{nome},{email},{endereco}\n")
