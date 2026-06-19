fd1ffa2c0269520cb7ff9288279d9d6d5ab5d962
Uriel <uriellongo0@gmail.com>
2026-06-18 21:08:42 -0300

    mudanças no codigo do pedidos

--- funcao_criar_pedido.py
+++ funcao_criar_pedido.py
@@ -1,6 +1,27 @@
 def criar_pedido():
     while True:
         pedido = input("Digite o pedido que deseja criar: ")
-        with open('C:\\Users\\uriel61918066\\Documents\\SIMPA\\pedidos.csv', 'a', encoding='utf-8') as arquivo:
-            arquivo.write(f"{pedido}\n")
-            print("Pedido criado com sucesso!")
+        
+
+"""
+ pedidos = {
+                "1": {
+                    "cliente": "João",
+                    "entrega": "Rua A, 123",
+                    "status": "Em andamento",
+                    "produtos": [
+                        "1" : {015;Escultura em Madeira;Artesanato;150.00;3;Thiago Ribeiro},
+                        "2" : {015;Escultura em Madeira;Artesanato;150.00;3;Thiago Ribeiro1}
+                    ]
+                }
+ }
+
+pedidos["1"]["cliente"] 
+      pedidos["1"]["cliente"]["produtos"]
+pedidos[numero_selecionado_digitado]["status"] 
+
+print(pedidos[numero_selecionado_digitado]["produtos"])
+produto_para_remover = 1
+pedidos[numero_selecionado_digitado]["produtos"].pop(produto_para_remover)
+
+"""
